879bf658f8c617c5edda142db244ba000017f9ce
Roman <slyusarrv@yandex.ru>
2021-10-06 18:31:54 +0500

    plots and colormaps

--- colormap_points.py
+++ colormap_points.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 1001))
+x_values = list(range(1001))
 y_values = [x ** 2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-# c - RGB в диапазоне от 0 до 1
-ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Назначение диапазона для каждой оси.
 ax.axis([0, 1100, 0, 1100000])
 
+# plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
