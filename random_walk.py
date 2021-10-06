37801779a5a13925a65c8eaecb8a4e9a6c7b5ff2
Roman <slyusarrv@yandex.ru>
2021-10-06 20:12:31 +0500

    random walk visualization

--- random_walk.py
+++ random_walk.py
@@ -17,15 +17,9 @@ class RandomWalk:
 
         # Шаги генерируются до достижения нужной длины.
         while len(self.x_values) < self.num_points:
-            # Определение направления и длины перемещения.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
 
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # Отклонение нулевых перемещений.
             if x_step == 0 and y_step == 0:
@@ -37,3 +31,11 @@ class RandomWalk:
 
             self.x_values.append(x)
             self.y_values.append(y)
+
+    def get_step(self):
+        """Определение направления и длины перемещения."""
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        step = direction * distance
+
+        return step
